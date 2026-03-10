08060069e8a7f14540e0b20d0bad6ffc55734e6f
SinofPride-999 <jhaycodes999@gmail.com>
2026-03-10 14:49:31 +0000

    feat: server starter

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/SinofPride-999/RSS-Aggregator
 
 go 1.25.0
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello Universe")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(".env files could not be loaded")
+	}
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the enviroment.")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
