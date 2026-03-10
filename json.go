8a43fc8b1776a730a758ac95f140baa897d402e4
SinofPride-999 <jhaycodes999@gmail.com>
2026-03-10 15:05:51 +0000

    feat: json helper for responding with json data

---
+++ json.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal JSON response: %v", payload)
+		w.WriteHeader(500)
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+
+	_, err = w.Write(data)
+	if err != nil {
+		log.Printf("Failed to write data: %v", err)
+	}
+}
