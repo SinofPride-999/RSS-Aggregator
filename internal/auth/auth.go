d8c344fc5d253c1602d1ffdc3cba9b64455ebc76
SinofPride-999 <jhaycodes999@gmail.com>
2026-03-16 21:11:38 +0000

    fix: getAPIKey string split

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -14,7 +14,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("no authentication info found")
 	}
 
-	vals := strings.Split(val, "")
+	vals := strings.Split(val, " ")
 	if len(vals) != 2 {
 		return "", errors.New("malformed auth header")
 	}
